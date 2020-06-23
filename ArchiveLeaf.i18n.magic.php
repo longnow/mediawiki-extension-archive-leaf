a61cee5b1c935723e0a3845677f233e4287d4c54
David Kamholz <lautgesetz@gmail.com>
2020-06-23 13:43:19 -0700

    reorganize for toolforge tool adaptation

--- ArchiveLeaf.i18n.magic.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$magicWords = array();
-
-/** English (English) */
-$magicWords['en'] = array(
-    'sanitize_leaf_title' => array( 0, 'sanitize_leaf_title' ),
-);
--- modules/ext.archiveleaf.special.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-    $('.archive-leaf-form').submit(function(event){
-
-        $('#import_id').hide();
-        $('#import_submit').hide();
-        $('#loading_form').fadeIn();
-
-    });
-});
