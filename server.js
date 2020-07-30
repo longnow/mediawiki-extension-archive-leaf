9f0e4b6550c3adcad208d252ee638c2b06ba5afa
David Kamholz <lautgesetz@gmail.com>
2020-07-30 14:49:17 -0700

    update/cleanup

--- server.js
+++ server.js
@@ -1,18 +1,35 @@
-const apiBaseUrl = 'https://wikisource.org/';
+const config = require('./config.json');
 
-const path = require('path');
 const fastify = require('fastify')({
   logger: true,
 });
-fastify.register(require('fastify-static'), {
-  root: path.join(__dirname, 'public'),
-});
-fastify.register(require('fastify-reply-from'), {
-  base: apiBaseUrl,
-});
 
-fastify.get('/api.php', (request, reply) => {
-  reply.from('/w/api.php');
+fastify
+  .register(require('fastify-cookie'))
+  .register(require('fastify-session'))
+  .register(require('fastify-oauth2'), {
+    name: 'wikisourceOAuth2',
+    credentials: {
+      client: {
+        id: config.consumerKey,
+        secret: config.consumerSecret
+      },
+      auth: {
+        authorizeHost: config.apiBaseUrl,
+        authorizePath: '/w/rest.php/oauth2/authorize',
+        tokenHost: config.apiBaseUrl,
+        tokenPath: '/w/rest.php/oauth2/access_token'
+      }
+    },
+    callbackUri: 'http://localhost:3000/callback'
+  })
+  .register(require('fastify-static'), {
+    root: `${__dirname}/public`,
+  });
+
+fastify.get('/callback', async function (req, reply) {
+  const token = await this.wikisourceOAuth2.getAccessTokenFromAuthorizationCodeFlow(req);
+  reply.send({ access_token: token.access_token });
 });
 
 fastify.listen(process.env.PORT || 3000, (err, address) => {
