64b2ccec24973efc759ce65cc0639afbb58ade35
David Kamholz <lautgesetz@gmail.com>
2020-08-02 21:01:42 -0700

    font fixes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import transliterators from "./transliterator.json";
 
 const scriptFont = {
   "bali": {
-    "fonts": ["Kadiri","Natya","Pustaka_Bali","Vimala"],
+    "fonts": ["Kadiri","Pustaka_Bali","Vimala"],
     "default": "Vimala"
   }
 };
